b138f4bced053d21881870d5b7aadfb12854be67
andresmurilloco <andresmurillocortazar@outlook.com>
2023-05-25 11:01:31 -0500

    Lesson 381

--- layout/Layout.js
+++ layout/Layout.js
@@ -1,7 +1,24 @@
 import Head from "next/head"
+import Modal from "react-modal"
 import Sidebar from "@/components/Sidebar"
+import useQuiosco from "@/hooks/useQuiosco";
+
+const customStyles = {
+    content: {
+      top: '50%',
+      left: '50%',
+      right: 'auto',
+      bottom: 'auto',
+      marginRight: '-50%',
+      transform: 'translate(-50%, -50%)',
+    },
+  };
+
+  Modal.setAppElement("#__next")
 
 export default function Layout({children, pagina}){
+    const {modal} = useQuiosco();
+
     return( 
     <>
         <Head>
@@ -17,6 +34,11 @@ export default function Layout({children, pagina}){
                     {children}
                 </div>
             </main>
+            {modal && ( 
+            <Modal isOpen={modal}
+            style={customStyles}>
+                <h1>Modal..</h1>
+            </Modal>)}
         </div>
     </>)
 }
